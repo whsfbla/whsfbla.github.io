912530c86ceb93c40291142be8be9aeeea81b1df
David M. Pham <riemannian.hypersquares@sapereaude.attlocal.net>
2017-04-10 22:14:50 -0500

    Solid 3

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,15 @@
     });
 
 })(jQuery);
+
+//tabs
+$('.tabContent').removeClass('activeTab');
+$('#tab1').addClass('activeTab');
+$('.tabs a').click(function() {
+  $('.tabs a').removeClass('active');
+  $(this).addClass('active');
+  var currentTab = $(this).attr('href');
+  $('.tabContent').removeClass('activeTab');
+  $(currentTab).addClass('activeTab');
+  return false;
+});
